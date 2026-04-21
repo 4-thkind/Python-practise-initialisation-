cbcbe397c90dada7268c7ed0c5f18fdaefcbdcd5
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-04-21 13:23:42 +0000

    Defining fraction class

--- Self DataTYpe/FractionDataType.py
+++ Self DataTYpe/FractionDataType.py
@@ -1,3 +1,7 @@
 #THERE IS NOT PARTICULARLY PYTHON DATA TYPE FOR FRACTION
 #SO I CREATED A CLASS TO REPRESENT IT
 
+class Fraction:
+    def __init__(self,n,d):
+        self.num= n
+        self.den=d
