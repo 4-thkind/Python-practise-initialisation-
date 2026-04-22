30793ec58d94ea629d6656ecfe8607e5c930b819
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-04-22 13:38:45 +0000

    created basic gui

--- GUI PROTOTYPE FOR LOGIN /login.py
+++ GUI PROTOTYPE FOR LOGIN /login.py
@@ -1,3 +1,11 @@
 from tkinter import *
 from PIL import ImageTk, Image, ImageSequence
 from tkinter import messagebox
+
+root = Tk()
+root.title("First Login Window")
+root.geometry("500x650")
+root.configure(background="brown")
+
+
+root.mainloop() 
