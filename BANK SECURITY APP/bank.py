23d4f40fb062fd07ff83700746e1fee73dd34837
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-04-23 15:47:07 +0000

    creating an ATM class containing pin and balance and card number of customer

--- BANK SECURITY APP/bank.py
+++ BANK SECURITY APP/bank.py
@@ -1,3 +1,12 @@
 from tkinter import *
 from PIL import ImageTk, Image, ImageSequence
 from tkinter import messagebox
+
+class Atm:
+    __counter=0
+    def __init__(self):
+        self.__pin=""
+        self.__balance=100000
+        self.CARD_NO=Atm.__counter
+        Atm.__counter+=1
+        self.menu()
