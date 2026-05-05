3dce75d0af5eaf4efbfbf6c8720eac628b318969
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-05-05 08:01:13 +0000

    created a result section to show final output

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -6,3 +6,7 @@ root.title("Calculator")
 root.geometry("280x380")
 root.configure(bg="black")
 root.resizable(0,0)
+
+result_lable=Label(root,text="",bg="black",fg="white")
+result_lable.grid(column=0,row=0,pady=(50,25),sticky="w",columnspan=7)
+result_lable.configure(font=("verdana",30,"bold"))
