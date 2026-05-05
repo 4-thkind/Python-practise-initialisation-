ccbdb43f646cd16905080eefb784cfe630846b42
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-05-05 08:06:12 +0000

    added a 7 number button

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -10,3 +10,7 @@ root.resizable(0,0)
 result_lable=Label(root,text="",bg="black",fg="white")
 result_lable.grid(column=0,row=0,pady=(50,25),sticky="w",columnspan=7)
 result_lable.configure(font=("verdana",30,"bold"))
+
+btn7 = Button(root,text='7',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_digit(7))
+btn7.grid(row=1,column=0)
+btn7.config(font=('verdana',14))
