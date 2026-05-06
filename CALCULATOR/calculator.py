ad342bbfd2c1361148ece3c53e4e757f8169de6d
Utkarsh Singh <utkarshsingh72007@gmail.com>
2026-05-06 06:03:10 +0000

    added division button; this completes the ui of our calculator

--- CALCULATOR/calculator.py
+++ CALCULATOR/calculator.py
@@ -70,3 +70,9 @@ btn0.config(font=('verdana',14))
 btn_equals = Button(root,text='=',bg='#00a65a',fg='white',width=5,height=2,command=get_result)
 btn_equals.grid(row=4,column=2)
 btn_equals.config(font=('verdana',14))
+
+btn_div = Button(root,text='/',bg='#00a65a',fg='white',width=5,height=2,command=lambda :get_operator('/'))
+btn_div.grid(row=4,column=3)
+btn_div.config(font=('verdana',14))
+
+root.mainloop()
